333580bfe118877f90f395aa5df17c09c8a302c5
PeterQLee <leep1995@gmail.com>
2015-03-28 14:50:45 -0300

    Created server and message sending

--- Contacts.java
+++ Contacts.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 public class Contacts {
     //housescontacts for the GUI, makes it simpler for user
 
     private ArrayList<String> address;
     private ArrayList<String> name;
+
     public Contacts() {
+	
     }
     public void addContact(String name, String address) {
 	//adds contacts to arraylist
@@ -15,5 +18,6 @@ public class Contacts {
 	//returns arraylist of contacts
 	//in order of name, then address
 	//e.g. [Bob,192.168.2.1,Charley,127.0.0.1,Cameron,192.222.3.1..etc]
+	return null;
     }
 }
